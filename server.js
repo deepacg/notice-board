818ee53a068e86060b90e5a173a5eeb986d3d473
Deepa George <54993216+deepacg@users.noreply.github.com>
2021-09-06 19:37:51 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/public/board.html')
+})
+
+app.get('/admin', (req, res)=>{
+    res.sendFile(__dirname+'/public/admin.html')
+})
+
+io.on('connection',(socket)=>{
+    console.log('new connection established')
+
+    socket.on('disconnect',()=>{
+        console.log('connection closed')
+    })
+
+    socket.on('message',(msg)=>{
+        console.log(msg)
+        io.emit('board_content', msg)
+    })
+})
+
+http.listen(3000, ()=>{
+    console.log('connected to server')
+})
